2fc0bdfc133731c6d49fc9f550386c72ade730cc
Mary Monchari Gichana <marygichana95@gmail.com>
2024-09-25 13:47:38 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+
+function mondayWork(place = 'go to the office'){
+    return `This Monday, I will ${place}.`;
+}
+mondayWork();
+function wrapAdjective(flair = "*"){
+    return function (nice = "special"){
+        return `You are ${flair}${nice}${flair}!`
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!")
